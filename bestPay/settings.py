7b4120f47af266a868ccfd9a5207f488573c8ff9
mikegyamfi <codeage20@gmail.com>
2023-02-09 12:51:01 +0000

    fourth commit

--- bestPay/settings.py
+++ bestPay/settings.py
@@ -132,10 +132,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'assets/'
-STATICFILES_DIRS = [BASE_DIR/'static/']
+STATIC_DIR = BASE_DIR/'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/'static/'
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
